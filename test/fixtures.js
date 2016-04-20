ea7d29c2cde4673bfe86421e83d1f5b7875c4bf5
Louis Barranqueiro <louis.barranqueiro@gmail.com>
2016-04-20 23:09:31 +0200

    add fixtures for test

---
+++ test/fixtures.js
@@ -0,0 +1,14 @@
+var content = [
+  'hey buddy, it\'s a test[^1]',
+  'here is an inline footnote[^2](inline footnote)',
+  'and another one[^3]',
+  'and another one[^4]',
+  '[^1]: basic footnote content',
+  '[^3]: paragraph\nfootnote\ncontent',
+  '',
+  '[^4]: footnote content 4'
+].join('\n');
+
+module.exports = {
+  content:content
+};
