1194ad71f1000d787a7b4794c1cd48db8bbe468e
Louis Barranqueiro <louis.barranqueiro@gmail.com>
2016-07-21 23:31:39 +0200

    #1 - fix bug with random number (add tests)

--- test/fixtures.js
+++ test/fixtures.js
@@ -1,9 +1,9 @@
 var content = [
-  'hey buddy, it\'s a test[^1]',
+  'hey buddy, it\'s a test[^5]',
   'here is an inline footnote[^2](inline footnote)',
   'and another one[^3]',
   'and another one[^4]',
-  '[^1]: basic footnote content',
+  '[^5]: basic footnote content',
   '[^3]: paragraph\nfootnote\ncontent',
   '',
   '[^4]: footnote content with some [markdown](https://en.wikipedia.org/wiki/Markdown)4'
--- test/footnotes.spec.js
+++ test/footnotes.spec.js
@@ -1,26 +1,63 @@
 /* eslint-disable no-unused-vars */
 var should = require('chai').should();
 /* eslint-enable no-unused-vars */
-var fixtures = require('./fixtures');
 var footnotes = require('./../src/footnotes');
 
 describe('footnotes', function() {
-  it('render', function() {
-    var content = footnotes(fixtures);
-    content.should.equal('hey buddy, it\'s a test' +
-      '<sup id="fnref:1"><a href="#fn:1" rel="footnote">1</a></sup>' +
-      '\nhere is an inline footnote' +
-      '<sup id="fnref:2"><a href="#fn:2" rel="footnote">2</a></sup>' +
-      '\nand another one' +
-      '<sup id="fnref:3"><a href="#fn:3" rel="footnote">3</a></sup>' +
-      '\nand another one<sup id="fnref:4"><a href="#fn:4" rel="footnote">4</a></sup>' +
-      '\n\n\n' +
-      '<div id="footnotes"><hr><div id="footnotelist"><ol>' +
-      '<li id="fn:1">basic footnote content<a href="#fnref:1" rev="footnote"> ↩</a></li>' +
-      '<li id="fn:2">inline footnote<a href="#fnref:2" rev="footnote"> ↩</a></li>' +
-      '<li id="fn:3">paragraph\nfootnote\ncontent<a href="#fnref:3" rev="footnote"> ↩</a></li>' +
-      '<li id="fn:4">footnote content with some ' +
-      '<a href="https://en.wikipedia.org/wiki/Markdown">markdown</a>' +
-      '4<a href="#fnref:4" rev="footnote"> ↩</a></li></ol></div></div>');
+  it('render (basic)', function() {
+    var content = footnotes('hey buddy[^1], it\'s a test [^1]: basic footnote content');
+    content.should.equal(
+      'hey buddy<sup id="fnref:1"><a href="#fn:1" rel="footnote">1</a></sup>, it\'s a test ' +
+      '<div id="footnotes">' +
+      '<hr>' +
+      '<div id="footnotelist">' +
+      '<ol style="list-style:none; padding-left: 0;">' +
+      '<li id="fn:1">' +
+      '<span style="display: inline-block; vertical-align: top; padding-right: 10px;">' +
+      '1.' +
+      '</span>' +
+      '<span style="display: inline-block; vertical-align: top;">' +
+      'basic footnote content' +
+      '</span>' +
+      '<a href="#fnref:1" rev="footnote"> ↩</a></li></ol></div></div>'
+    );
+  });
+
+  it('render (random number)', function() {
+    var content = footnotes('hey buddy[^13], it\'s a test [^13]: basic footnote content');
+    content.should.equal(
+      'hey buddy<sup id="fnref:13"><a href="#fn:13" rel="footnote">13</a></sup>, it\'s a test ' +
+      '<div id="footnotes">' +
+      '<hr>' +
+      '<div id="footnotelist">' +
+      '<ol style="list-style:none; padding-left: 0;">' +
+      '<li id="fn:13">' +
+      '<span style="display: inline-block; vertical-align: top; padding-right: 10px;">' +
+      '13.' +
+      '</span>' +
+      '<span style="display: inline-block; vertical-align: top;">' +
+      'basic footnote content' +
+      '</span>' +
+      '<a href="#fnref:13" rev="footnote"> ↩</a></li></ol></div></div>'
+    );
+  });
+
+  it('render (with markdown content)', function() {
+    var content = footnotes('hey buddy[^13], it\'s a test [^13]: basic footnote [content](http://example.com)');
+    content.should.equal(
+      'hey buddy<sup id="fnref:13"><a href="#fn:13" rel="footnote">13</a></sup>, it\'s a test ' +
+      '<div id="footnotes">' +
+      '<hr>' +
+      '<div id="footnotelist">' +
+      '<ol style="list-style:none; padding-left: 0;">' +
+      '<li id="fn:13">' +
+      '<span style="display: inline-block; vertical-align: top; padding-right: 10px;">' +
+      '13.' +
+      '</span>' +
+      '<span style="display: inline-block; vertical-align: top;">' +
+      'basic footnote <a href="http://example.com">content</a>' +
+      '</span>' +
+      '<a href="#fnref:13" rev="footnote"> ↩</a></li></ol></div></div>'
+    );
   });
 });
